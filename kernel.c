b63aa375050ed38f3184226625c6e7de98a19b71
Curtis McEnroe <programble@gmail.com>
2016-11-14 23:49:30 -0500

    Hello, world!

--- kernel.c
+++ kernel.c
@@ -17,4 +17,11 @@ static const int COLS = 80;
 static u16 *vram = (u16 *)0xB8000;
 
 void kmain(void) {
+    for (int i = 0; i < ROWS * COLS; ++i)
+        vram[i] = 0;
+
+    const char hello[] = "Hello, world!";
+    int i = 0;
+    for (const char *c = hello; *c; ++c)
+        vram[i++] = BLUE << 12 | (BRIGHT | GRAY) << 8 | *c;
 }
