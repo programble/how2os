f761df75b4f92f64fac437a0daf00344192e6777
Curtis McEnroe <programble@gmail.com>
2016-11-14 23:45:09 -0500

    Add vram definitions

--- kernel.c
+++ kernel.c
@@ -1,2 +1,20 @@
+typedef unsigned short u16;
+
+enum {
+    BLACK,
+    BLUE,
+    GREEN,
+    CYAN,
+    RED,
+    MAGENTA,
+    YELLOW,
+    GRAY,
+    BRIGHT,
+};
+
+static const int ROWS = 25;
+static const int COLS = 80;
+static u16 *vram = (u16 *)0xB8000;
+
 void kmain(void) {
 }
